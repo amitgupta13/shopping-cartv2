d3981070123f9ea005e80a46e4df004ff5fe0b14
Amit Gupta <amitkrgupta13@gmail.com>
2018-12-22 12:54:07 +0530

    nc

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const {Product} = require('../models/product');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+router.get('/', async function(req, res, next) {
+  const products = await Product.find();
+  res.render('shop/index', { products });
 });
 
 module.exports = router;
